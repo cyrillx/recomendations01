11c79ed29aed8893f6fc5be074d7162c1438ca2c
Kirill <cyrillx@users.noreply.github.com>
2021-07-17 16:14:24 +0300

    Added project files

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Alarm : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _penetration;
+    [SerializeField] private float _deltaVolume;
+    [SerializeField] private AudioSource _alarmSound;
+
+    private float _currentVolume = 0;
+    private float _targetVolume;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _targetVolume = 1f;
+        _alarmSound.Play();
+        _alarmSound.loop = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _targetVolume = 0f;
+    }
+
+    private void Update()
+    {
+        _currentVolume = Mathf.MoveTowards(_currentVolume, _targetVolume, _deltaVolume);
+        _alarmSound.volume = _currentVolume;
+        if (_currentVolume == 0)
+            _alarmSound.loop = false;
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(_speed * Time.deltaTime, 0, 0);
+            _spriteRenderer.flipX = false;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(- _speed * Time.deltaTime, 0, 0);
+            _spriteRenderer.flipX = true;
+        }
+    }
+}
